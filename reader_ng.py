e355af424d047047f73fbc81d65c525792b39b92
FLC99 <francescolacorte99@gmail.com>
2026-07-20 18:03:06 +0200

    read parameters almost finished

--- reader_ng.py
+++ reader_ng.py
@@ -176,31 +176,36 @@ def load_config(name: str) -> Dict[str, Any]:
             ref_count = config.get("multiple_amount_ref", 0) if with_ref else 0
             sample_count = config.get("multiple_amount", 0) if config.get("with_multiple", False) else 0
             folders = [Path(p) for p in config.get("folders", [])]
+            topspin_folders = [Path(p) for p in config.get("topspin_folders", [])]
             groups = []
             idx = 0
             if with_ref and ref_count > 0:
                 groups.append({
                     "label": "reference",
                     "is_reference": True,
-                    "folders": folders[idx:idx+ref_count]
+                    "folders": folders[idx:idx+ref_count],
+                    "topspin_folders": topspin_folders[idx:idx+ref_count]
                 })
                 idx += ref_count
             if sample_count > 0:
                 groups.append({
                     "label": "sample",
                     "is_reference": False,
-                    "folders": folders[idx:idx+sample_count]
+                    "folders": folders[idx:idx+sample_count],
+                    "topspin_folders": topspin_folders[idx:idx+sample_count]
                 })
             config["groups"] = groups
             # Remove old keys
             for old_key in ("with_ref", "with_multiple", "multiple_amount",
-                            "multiple_amount_ref", "folders"):
+                            "multiple_amount_ref", "folders", "topspin_folders"):
                 config.pop(old_key, None)
         else:
             # Ensure folders and files are Path objects
             for grp in config["groups"]:
                 if "folders" in grp:
                     grp["folders"] = [Path(p) for p in grp.get("folders", [])]
+                if "topspin_folders" in grp:
+                    grp["topspin_folders"] = [Path(p) for p in grp.get("topspin_folders", [])]
                 if "files" in grp:
                     grp["files"] = [Path(p) for p in grp.get("files", [])]
         return config
@@ -415,37 +420,49 @@ def find_maximum(arr: np.ndarray,
     max_idx = sub_arr.argmax() + start
     return float(max_val), int(max_idx)
 
-def parameter_extract(file_path: Path, PARAMETER: str) -> List[float]:
+def parameter_extract(file_path: Path, PARAMETER: str = None) -> List[float]:
     if not file_path.exists():
         raise FileNotFoundError(colored(
             f"{file_path} not found.", "red", attrs=["bold"])
         )
     text = file_path.read_text(encoding="utf-8", errors="ignore")
 
-    # Look for the header and also capture the block up to the next '##$'
-    hdr_pattern = rf"##\${PARAMETER}=\s*\(\s*(?P<N>\d+)\s*\)\s*\n(?P<block>.*?)(?=\n##\$|\Z)"
-    match = re.search(hdr_pattern, text, re.DOTALL)
-    if not match:
-        raise ValueError(colored(
-            f"Header '##${PARAMETER}=( N )' non trovato in {file_path}.", "red", attrs=["bold"])
-        )
+    if PARAMETER is not None:
+        # Look for the header and also capture the block up to the next '##$'
+        hdr_pattern = rf"##\${PARAMETER}=(?:\s*\(\s*(?P<N>\d+)\s*\)\s*\n(?P<block>.*?)(?=\r?\n##\$|\Z)|\s*(?P<value>[^\r\n]*))"
+        match = re.search(hdr_pattern, text, re.DOTALL)
+        if not match:
+            raise ValueError(colored(
+                f"Header '##${PARAMETER}=( N )' non trovato in {file_path}.", "red", attrs=["bold"])
+            )
 
-    N = int(match.group("N"))
-    block = match.group("block")
-    print(f"{PARAMETER} dimension: {N}")
+        if match.group("N") is not None:
+            N = int(match.group("N"))
+        if match.group("block") is not None:
+            block = match.group("block")
+        if match.group("value") is not None:
+            val = match.group("value")
 
-    # Extract numbers only from this block
-    num_pattern = r"[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?"
-    vals = re.findall(num_pattern, block)
-    if len(vals) < N:
-        raise ValueError(colored(
-            f"Trovati solo {len(vals)} numeri nel blocco, attesi {N}.", "red", attrs=["bold"])
-        )
-    if len(vals) > N:
-        print(colored(
-            f"Attenzione: trovati {len(vals)} numeri nel blocco (attesi {N}), uso i primi {N}.", "yellow")
-        )
-    return [float(v) for v in vals[:N]]
+        if val is not None:
+            print(f"{PARAMETER} value: {val}")
+            return [float(val)]
+        else:
+            print(f"{PARAMETER} dimension: {N}")
+
+            # Extract numbers only from this block
+            num_pattern = r"[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?"
+            vals = re.findall(num_pattern, block)
+            if len(vals) < N:
+                raise ValueError(colored(
+                    f"Trovati solo {len(vals)} numeri nel blocco, attesi {N}.", "red", attrs=["bold"])
+                )
+            if len(vals) > N:
+                print(colored(
+                    f"Attenzione: trovati {len(vals)} numeri nel blocco (attesi {N}), uso i primi {N}.", "yellow")
+                )
+            return [float(v) for v in vals[:N]]
+    else: #TODO: implement the case when PARAMETER is None, if needed
+        pass
 
 def apply_phase(data: np.ndarray, p0: float, p1: float) -> np.ndarray:
     """
@@ -1405,11 +1422,20 @@ def select_experiment_folder(title="Select a folder") -> Path:
     root.withdraw()
     return Path(filedialog.askdirectory(title=title))
 
-def extract_parameters(folder: Path) -> Tuple[List[float], List[float]]:
-    method = folder / "method"
-    sat_hz = parameter_extract(method, "PVM_SatTransFL")
-    offset_hz = parameter_extract(method, "PVM_FrqWorkOffset")
-    return sat_hz, offset_hz
+def extract_parameters(folder: Path, filename: str) -> Tuple[List[float], List[float]]:
+    file = folder / filename
+    if filename == "method":
+        sat_hz = parameter_extract(file, "PVM_SatTransFL")
+        offset_hz = parameter_extract(file, "PVM_FrqWorkOffset")
+        return sat_hz, offset_hz
+    elif filename == "acqu2":
+        offset_hz = parameter_extract(file, "SFO1")
+        return offset_hz
+    elif filename == "fq2list":
+        sat_hz = parameter_extract(file)
+        return sat_hz
+    else:
+        raise ValueError(f"Unknown parameter file: {filename}")
 
 def load_spectra(folder: Path):
     dic, data = ng.bruker.read(folder)
@@ -1472,7 +1498,9 @@ def ensure_complete_config(config_name: str, config_data: Dict[str, Any]) -> Dic
                 cnt = ask_int("Number of folders", min_val=1, default=1)
                 topspin_folders = []
                 for _ in range(cnt):
-                    topspin_folders.append(select_experiment_folder())
+                    tmp_path: Path = select_experiment_folder()
+                    topspin_folders.append(tmp_path)
+                    #topspin_folders.append(select_experiment_folder())
                 return {"label": label, "is_reference": is_ref, "folders": [], "topspin_folders": topspin_folders, "files": []}
             else:  # Text files
                 cnt = ask_int("Number of text files", min_val=1, default=1)
@@ -1724,8 +1752,8 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                     
         if is_folder:   # BRUKER data
             folders = entries
-            for file in folders:
-                base_name = f"{file.parent.name[:12]}…{file.parent.name[-12:]}-{file.stem}"
+            for folder in folders:
+                base_name = f"{folder.parent.name[:12]}…{folder.parent.name[-12:]}-{folder.stem}"
                 folder_name_short = base_name
                 counter = 1
                 while folder_name_short in analysis_results:
@@ -1734,7 +1762,7 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                 analysis_results[folder_name_short] = {}
                 folder_keys_per_group[grp_idx].append(folder_name_short)
 
-                sat_trans_hz, work_offset_hz = extract_parameters(file)
+                sat_trans_hz, work_offset_hz = extract_parameters(folder, "method")
                 analysis_results[folder_name_short]["sat_trans_hz"] = sat_trans_hz
                 analysis_results[folder_name_short]["work_offset_hz"] = work_offset_hz
 
@@ -1749,7 +1777,7 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                         )
                         #return
 
-                dic, data, uc, ppm_axis, n_exp, bf1 = load_spectra(file)
+                dic, data, uc, ppm_axis, n_exp, bf1 = load_spectra(folder)
                 analysis_results[folder_name_short]["uc"] = uc
                 analysis_results[folder_name_short]["bf1"] = bf1
                 if group_meta[grp_idx].get("uc") is None:
@@ -1765,7 +1793,114 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                     window_title=f"{label}: Spectra  for {folder_name_short}"
                 )
 
-                if ppm_missing and grp_idx == 0 and file == folders[0]:
+                if ppm_missing and grp_idx == 0 and folder == folders[0]:
+                    plt.pause(0.05)
+                    start_ppm, end_ppm = ask_user_for_ppm_range()
+                    config["start_ppm"] = start_ppm
+                    config["end_ppm"] = end_ppm
+                    config["ppm_missing"] = False
+                    ppm_missing = False
+                    if config_name:
+                        save_config(config_name, config)
+
+                start_idx = ppm_to_index(uc, end_ppm)
+                end_idx = ppm_to_index(uc, start_ppm)
+
+                max_vals: List[float] = []
+                max_indexes: List[int] = []
+                global_max: float
+                global_min: float
+                max_vals, max_indexes, global_max, global_min = find_max_vals(spectra, start_idx, end_idx)
+                max_vals = normalize_max_vals(max_vals=max_vals, global_max=global_max, global_min=global_min, )
+
+                # Correct saturation frequencies
+                zero_corrected_ppm: List[float] = correct_sat_frequencies(
+                    sat_trans_hz, 
+                    max_indexes,
+                    work_offset_hz, 
+                    uc, 
+                    bf1
+                )
+
+                # --- Sort by ppm ---
+                combined = list(zip(sat_trans_hz, max_indexes, max_vals, zero_corrected_ppm))
+                combined.sort()
+                sat_trans_hz[:], max_indexes[:], max_vals[:], zero_corrected_ppm[:] = zip(*combined)
+
+                res = process_zspectrum_and_integrals(max_vals, zero_corrected_ppm)
+                analysis_results[folder_name_short].update({
+                    "max_indexes": max_indexes,
+                    "max_vals": max_vals,
+                })
+                analysis_results[folder_name_short].update(res)
+                group_raw[grp_idx].append((max_indexes, max_vals, sat_trans_hz))
+
+                # --- Calculate integrals for this individual folder ---
+                
+                # After storing the results for the single folder, optionally plot it
+                plot_data(
+                    x=res["spline_fit_results"]["x"],
+                    y=res["spline_fit_results"]["y"],
+                    x_fit=res["spline_fit_results"]["x_fit"],
+                    y_fit=res["spline_fit_results"]["y_fit"],
+                    title=f" {label}: {folder_name_short}",
+                    invert_x=True,
+                    add_lorentz=True,
+                    lorentzian_envelope_results=res["lorentzian_envelope_results"],
+                    add_sigmoid=True,
+                    sigmoidal_envelope_results=res["sigmoidal_envelope_results"],
+                    diff_x=res["diff_x"],
+                    diff_y=res["diff_y"],
+                    diff_label="Lorentzian envelope - Spline fit",
+                    visibility=config.get("plot_visibility", get_default_visibility()),
+                    window_title=f" {label}: spline fit for {folder_name_short}"
+                )
+        elif is_topspin_folder:   # BRUKER data spectroscopy
+            folders = entries
+            for folder in folders:
+                base_name = f"{folder.parent.name[:12]}…{folder.parent.name[-12:]}-{folder.stem}"
+                folder_name_short = base_name
+                counter = 1
+                while folder_name_short in analysis_results:
+                    folder_name_short = f"{base_name}_{counter}"
+                    counter += 1
+                analysis_results[folder_name_short] = {}
+                folder_keys_per_group[grp_idx].append(folder_name_short)
+
+                work_offset_hz = extract_parameters(folder, "acqu2")
+                sat_trans_hz = extract_parameters(folder, "fq2list")
+
+                analysis_results[folder_name_short]["sat_trans_hz"] = sat_trans_hz
+                analysis_results[folder_name_short]["work_offset_hz"] = work_offset_hz
+
+                if group_meta[grp_idx].get("work_offset_hz") is None:
+                    group_meta[grp_idx]["work_offset_hz"] = work_offset_hz
+                else:
+                    if group_meta[grp_idx]["work_offset_hz"] != work_offset_hz:
+                        print(colored(
+                            f"Error: different work_offset in group '{label}'", 
+                            "red", 
+                            attrs=["bold"])
+                        )
+                        #return
+
+                dic, data, uc, ppm_axis, n_exp, bf1 = load_spectra(folder)
+                analysis_results[folder_name_short]["uc"] = uc
+                analysis_results[folder_name_short]["bf1"] = bf1
+                if group_meta[grp_idx].get("uc") is None:
+                    group_meta[grp_idx]["uc"] = uc
+                    group_meta[grp_idx]["bf1"] = bf1
+
+                spectra = process_spectra(data, dic, n_exp)
+                fig = plot_spectra(
+                    title=f"{label} - {folder_name_short}",
+                    spectra=spectra, n_exp=n_exp, ppm_axis=ppm_axis,
+                    sat_trans_hz=sat_trans_hz,
+                    visibility=config.get("plot_visibility", get_default_visibility()),
+                    window_title=f"{label}: Spectra  for {folder_name_short}"
+                )
+
+                if ppm_missing and grp_idx == 0 and folder == folders[0]:
                     plt.pause(0.05)
                     start_ppm, end_ppm = ask_user_for_ppm_range()
                     config["start_ppm"] = start_ppm
@@ -1829,8 +1964,8 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                 )
         else:   # txt data files (sat_trans_hz vs max_vals) 
             files = entries
-            for file_idx, file in enumerate(files):
-                base_name = file.stem
+            for file_idx, folder in enumerate(files):
+                base_name = folder.stem
                 # Ensure unique key in analysis_results
                 key = base_name
                 counter = 1
@@ -1844,7 +1979,7 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                 try:
                     # Assume two columns: sat_trans_hz vs max_vals.
                     # Skip comments (lines starting with '#') and handle possible header.
-                    data = np.loadtxt(file, comments='#')
+                    data = np.loadtxt(folder, comments='#')
                     if data.ndim != 2 or data.shape[1] < 2:
                         raise ValueError("File must contain at least two columns.")
                     sat_trans_hz = data[:, 0].tolist()
@@ -1855,15 +1990,15 @@ def run_analysis(config_name: str, config: Dict[str, Any]) -> None:
                         value = config["groups"][grp_idx]["BF1"][file_idx]
                         group_meta[grp_idx]["bf1"] = value
                     except KeyError as e:
-                        print(colored(f"Missing key {e} for {file}", "red", attrs=["bold"]))
+                        print(colored(f"Missing key {e} for {folder}", "red", attrs=["bold"]))
                         continue
                     except (TypeError, IndexError) as e:
-                        print(colored(f"Invalid structure (expected dict/list) for {file}: {e}", "red", attrs=["bold"]))
+                        print(colored(f"Invalid structure (expected dict/list) for {folder}: {e}", "red", attrs=["bold"]))
                         continue
                                         
                     zero_corrected_ppm = [sat_trans_hz[i] / group_meta[grp_idx]["bf1"] for i in range(len(sat_trans_hz))]
                 except Exception as e:
-                    print(colored(f"Error reading file {file}: {e}", "red", attrs=["bold"]))
+                    print(colored(f"Error reading file {folder}: {e}", "red", attrs=["bold"]))
                     continue
 
                 max_indexes = [0] * len(max_vals)
